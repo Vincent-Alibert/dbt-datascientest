209513a923655e4a3f9978327285445130b3d6fa
Vincent Alibert <vincent.alibert@eurofiber.com>
2026-06-11 20:34:12 +0000

    loop

---
+++ models/COVID19_Epidemiological_Data/loop.sql
@@ -0,0 +1,11 @@
+select
+{%- for country in var('countries') %}
+{%- if ' ' not in country %}
+count(case when name = '{{country}}' then name end) as {{country}}_amount
+{%- else %}
+{% set country_underscore = country.replace(' ', '_') -%}
+count(case when name = '{{country}}' then name end) as {{country_underscore}}_amount
+{% endif -%}
+{%- if not loop.last %},{% endif -%}
+{% endfor -%}
+from {{ ref('country_codes') }}
